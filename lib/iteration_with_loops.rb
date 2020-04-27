43701c108c9c64ba11234d00b99060f2c96ca890
Darren Koomson <darrenkoomson17@gmail.com>
2020-04-27 22:19:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,6 +3,7 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   final = []
   count = 0
+  alpha = "abcdefghijklmnopqrstuvwxyz"
   
   while count < src.length
     if src[count].include?()
